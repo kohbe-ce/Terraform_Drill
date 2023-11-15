b704a1c0af0b902e5d0299b88b5a4f600707ab39
kohbe-ce <kohbe.ce@gmail.com>
2023-11-15 18:24:57 +0900

    2nd commit

--- 02/one-webserver/main.tf
+++ 02/one-webserver/main.tf
@@ -1,59 +1,80 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.25.0"
-    }
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
   }
 }
 
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 다음 부분을 새로 추가한다.
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
 
 provider "aws" {
   region = "us-east-2"
 }
 
-#Instance 생성 + user_data
-resource "aws_instance" "my-webserver" {
-  ami           = "ami-0e83be366243f524a"
-  instance_type = "t2.micro"
-
-  vpc_security_group_ids = [aws_security_group.my-webserver-sg.id]
+resource "aws_instance" "example" {
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
-                #!/bin/bash
-                apt -y install apache2
-                systemctl enable --now apache2
-                echo "hello, world" | sudo tee /var/www/html/index.html
-                EOF
-
-  user_data_replace_on_change = true
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
-    Name = "my-webserver"
+    Name = "terraform-example"
   }
 }
 
-# Security Group 생성
-resource "aws_security_group" "my-webserver-sg" {
-  name        = "my-webserver-sg"
-  description = "Allow ssh, http inbound traffic"
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
 
   ingress {
-    description      = "http from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh_tcp"
-  }  
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
 }
